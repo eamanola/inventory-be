3ad055ed7e316f756a1d8a37e690daf4a6517f10
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2021-05-04 14:01:49 +0300

    heroku debug

--- index.js
+++ index.js
@@ -11,19 +11,33 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('fe-build'));
 
+console.log('test');
+
 app.get('/category/:category', async (request, response) => {
+  console.log('test2');
   const { category } = request.params;
+  console.log('test3');
   const validValues = ['gloves', 'facemasks', 'beanies'];
+  console.log('test4');
 
   if (validValues.indexOf(category) === -1) {
-    return response.status(400).json({
+    console.log('test5');
+    response.status(400).json({
       error: `invalid category. valid values are ${validValues.join(', ')}`,
     });
+
+    return;
   }
 
-  const [categoryData, manufacturersData] = await fetchData(category);
-  const data = formatData(categoryData, manufacturersData);
-  return response.status(200).json(data);
+  try {
+    console.log('test6');
+    const [categoryData, manufacturersData] = await fetchData(category);
+    console.log('test7');
+    const data = formatData(categoryData, manufacturersData);
+    response.status(200).json(data);
+  } catch (e) {
+    response.status(500).json({ error: 'unknown' });
+  }
 });
 
 const { PORT } = config;
